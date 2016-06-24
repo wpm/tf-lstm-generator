b91d81dba22e8fca4c4b562986f941161426f02d
W.P. McNeill <wmcneill@us.ibm.com>
2016-06-24 17:09:27 -0500

    Use install_requires instead of requirements file

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ from setuptools import setup
 import ghostwriter
 
 setup(
-    name='ghostwriter',
+    name="ghostwriter",
     version=ghostwriter.__version__,
     packages=['ghostwriter'],
-    url='',
-    license='',
-    author='W.P. McNeill',
-    author_email='billmcn@gmail.com',
-    description='Machine-Assisted Writing',
+    url="",
+    license="",
+    author="W.P. McNeill",
+    author_email="billmcn@gmail.com",
+    description="Machine-Assisted Writing",
     entry_points={
-        'console_scripts': ['ghostwriter=ghostwriter.main:main'],
-    }
+        "console_scripts": ["ghostwriter=ghostwriter.main:main"],
+    },
+    install_requires=[
+        "tensorflow",
+        "nltk"
+    ]
 )
